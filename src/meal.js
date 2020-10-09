dccefffe8c68bae79d3b35fd5dd1be5e00ba2aad
Anneke Miers <annekemiers.am@gmail.com>
2020-10-09 17:10:52 -0600

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,30 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem =
+    {
+      name: name,
+      price: price,
+      type: type,
+    };
+
+    return menuItem
+}
+
+/*
+function addIngredients (ingredients) {
+  var ingredients = [ingredients]
+  if (ingredients)
+}
+*/
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  //addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-

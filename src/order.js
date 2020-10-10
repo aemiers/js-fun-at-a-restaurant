f2ebf587db96a3e5b2a5e546594aa08c78207d36
Anneke Miers <annekemiers.am@gmail.com>
2020-10-10 10:58:06 -0600

    Complete takeOrder function

--- src/order.js
+++ src/order.js
@@ -1,7 +1,15 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length <= 2)
+  deliveryOrders.push(order);
+}
+
+function refundOrder () {
+
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }

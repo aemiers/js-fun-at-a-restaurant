84ccb4e67be04ca3ba35e61b1bfa6ad5a3da8e04
Anneke Miers <annekemiers.am@gmail.com>
2020-10-09 20:11:21 -0600

    Complete assignment with passed tests

--- src/meal.js
+++ src/meal.js
@@ -12,18 +12,37 @@ function createMenuItem(name, price, type) {
     return menuItem
 }
 
-/*
-function addIngredients (ingredients) {
-  var ingredients = [ingredients]
-  if (ingredients)
+function addIngredients(topping, ingredients) {
+  var topping = topping;
+    if (!ingredients.includes(topping))
+      ingredients.push(topping)
 }
-*/
+
+function formatPrice(price) {
+  var formattedPrice = "$" + price
+  return  formattedPrice
+}
+
+function decreasePrice(price) {
+
+  var decreasedPrice = price - (price * .10)
+  return decreasedPrice
+}
+
+function createRecipe (title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
-  //addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }

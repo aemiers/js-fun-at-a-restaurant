fde65f94a04a044eaef1cb4a3939d1359033bd68
Anneke Miers <annekemiers.am@gmail.com>
2020-10-10 17:42:55 -0600

    Update first two functions, drafts for the rest of the functions

--- src/order.js
+++ src/order.js
@@ -3,13 +3,46 @@ function takeOrder(order, deliveryOrders) {
   deliveryOrders.push(order);
 }
 
-function refundOrder () {
-
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1)}
+  }
+  return deliveryOrders
 }
+
+// function listItems(deliveryOrders) {
+//   for (var i = 0; i < deliveryOrders.length; i++) {
+//     deliveryOrders[i].item = items
+//
+//   return items
+// }
+
+// function listItems(deliveryOrders) {
+//   for (var i = 0; i < deliveryOrders.length; i++) {
+//
+//     deliveryOrders.slice(0, i}
+//
+//   return items
+// }
+
+function listItems(deliveryOrders) {
+  var items = deliveryOrders.forEach(function(obj)){
+    a.push(obj.item);
+  }
+  return items
+}
+
+// function searchOrder(deliveryOrders, item) {
+//   for (var i = 0; i < deliveryOrders.length; i++) {
+//     if(deliverOrders[i].item !== item) {
+//       return false} else {return true}
+//     }
+// }
 
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
-  // searchOrder
+  listItems,
+  searchOrder
 }

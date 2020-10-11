9a51eeeed22cd08d49de13912199435ec3beb944
Anneke Miers <annekemiers.am@gmail.com>
2020-10-11 16:47:31 -0600

    Finish first function, draft on second and third

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,83 @@
 
+function createRestaurant(name) {
+  return restaurant =
+  {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+}
+
+// function addMenuItem(name, lunchItem) {
+//   restaurant.menus.lunch.push(lunchItem)
+// }
+
+
+// function addMenuItem(name, item) {
+//   if (item.type === "lunch") {
+//     restaurant.menus.lunch.push(item)
+//       } else {
+//         restaurant.menus.breakfast.push(item)
+//   }
+// }
+
+// function addMenuItem(name, item) {
+//   if (item.type === "lunch") {
+//     restaurant.menus.lunch.push(item)
+//       } else {
+//         restaurant.menus.breakfast.push(item)
+//   }
+// }
+// function addMenuItem(name, item) {
+//   if (!name.includes(item)) {
+//     name.menus.type.push(item)
+//       } else {
+//         restaurant.menus.type.push(item)
+//   }
+// }
+
+function addMenuItem(name, item) {
+  if (!name.menu.menutype.items.includes(item)) {
+    name.menus.type.push(item)
+      } else {
+        restaurant.menus.type.push(item)
+  }
+}
+
+// lunchItem.type === restaurant.menus
+// function addMenuItem(name, menuItem) {
+//   restaurant.menus.push(menuItem) {
+//     if (!restaurant.includes(menuItem)) {
+//       ingredients.push(menuItem)}
+//     }
+// }
+
+// function addMenuItem(name, menuItem) {
+//     if (!restaurant.includes(menuItem)) {
+//       ingredients.push(menuItem)}
+//     restaurant.type.push(menuItem)
+//
+// }
+
+
+
+// function removeMenuItem(item, type) {
+//   for (var i = 0; i < restaurant.length; i++) {
+//     if (
+// slice
+//       restaurant.menus.pop
+// }
+// }
+//
+//   return `No one is eating our ${item} - it has been removed from the ${type} menu!
+//
+//   {else} return `Sorry, we don't sell ${item}, try adding a new recipe!`
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  //removeMenuItem
 }
